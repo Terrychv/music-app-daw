4ad29a78fb9dc268f557358c72aa1e65d23ae9bb
Lauder182 <laurapv0304@hotmail.com>
2025-05-28 13:32:02 +0200

    creacion de los formularios con sus css, añadidos a los urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.catalogo_albums, name='catalogo_albums'),
     path('search/',views.buscar_contenido, name='buscar_contenido'),
+    # pagians detalladas de los elementos
     path('album/<int:album_id>/', views.detalle_album, name='detalle_album'),
     path('artist/<int:artist_id>/', views.detalle_artista, name='detalle_artista'),
     path('song/<int:song_id>/', views.detalle_cancion, name='detalle_cancion'),
+    # formularios para crear elementos
+    path('album/form/', views.form_album, name='form_album'),
+    path('artist/form/', views.form_album, name='form_artista'),
+    path('song/form/', views.form_album, name='form_cancion'),
 ]
